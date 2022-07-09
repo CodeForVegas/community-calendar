086f37c63cda68c03d24bfdb8f54cb9c598806af
Zachary Gonzalez <zacharygonzalez1234@gmail.com>
2022-07-09 12:01:35 -0700

    Screens and Routing Laid Out

--- community_calendar/lib/main.dart
+++ community_calendar/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:community_calendar/UI/Screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,11 +11,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Community Calendar',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      routeInformationParser: VxInformationParser(),
+      routerDelegate: VxNavigator(routes: {
+        '/': (_, __) => const MaterialPage(child: HomeScreen()),
+        '/login': (_, __) => const MaterialPage(child: HomeScreen()),
+        '/signup': (_, __) => const MaterialPage(child: HomeScreen()),
+      }),
     );
   }
 }
