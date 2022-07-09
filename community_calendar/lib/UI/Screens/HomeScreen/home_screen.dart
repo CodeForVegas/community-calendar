4f950625b4db0b4f1435622c7ce50f10949d15e0
Zachary Gonzalez <zacharygonzalez1234@gmail.com>
2022-07-09 13:09:44 -0700

    Login Screen UI Done

--- community_calendar/lib/UI/Screens/HomeScreen/home_screen.dart
+++ community_calendar/lib/UI/Screens/HomeScreen/home_screen.dart
@@ -1,6 +1,7 @@
-
 import 'package:community_calendar/UI/Univeral_Widgets/header.dart';
+import 'package:community_calendar/UI/Univeral_Widgets/textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,7 +10,19 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: [ Header()],
+        children: [
+          Header(),
+          CFVTextField(
+            hintText: 'Search....',
+            borderWidth: 1,
+            horizontalPadding: MediaQuery.of(context).size.width * 0.3,
+            verticalPadding: MediaQuery.of(context).size.width * 0.02,
+            suffixIcon: MdiIcons.magnify,
+            borderRadius: 3,
+            onChanged: (text) {},
+            onSumbit: (text) {},
+          )
+        ],
       ),
     );
   }
--- community_calendar/lib/UI/Screens/LoginScreen/login_screen.dart
+++ community_calendar/lib/UI/Screens/LoginScreen/login_screen.dart
@@ -1,15 +1,68 @@
-
+import 'package:community_calendar/UI/Univeral_Widgets/button.dart';
 import 'package:community_calendar/UI/Univeral_Widgets/header.dart';
+import 'package:community_calendar/UI/Univeral_Widgets/textfield.dart';
+import 'package:community_calendar/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-      body: ListView(
-        children: [ Header(showLogin: false,)],
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            Header(
+              showLogin: false,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.04,
+            ),
+            Constants.heading(
+                text: 'Login to the Community Calendar', fontSize: 32),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.13,
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CFVTextField(
+                    hintText: 'Email',
+                    borderWidth: 1,
+                    horizontalPadding: MediaQuery.of(context).size.width * 0.3,
+                    verticalPadding: MediaQuery.of(context).size.width * 0.02,
+                    suffixIcon: MdiIcons.account,
+                    borderRadius: 3,
+                    onChanged: (text) {},
+                    onSumbit: (text) {},
+                  ),
+                  CFVTextField(
+                    hintText: 'Password',
+                    borderWidth: 1,
+                    horizontalPadding: MediaQuery.of(context).size.width * 0.3,
+                    verticalPadding: MediaQuery.of(context).size.width * 0.02,
+                    suffixIcon: MdiIcons.eye,
+                    isObscure: true,
+                    borderRadius: 3,
+                    onChanged: (text) {},
+                    onSumbit: (text) {},
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            CFVButton(
+              text: 'Login',
+            )
+          ],
+        ),
       ),
     );
   }
